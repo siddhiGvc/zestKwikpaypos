193701ac3fed626c458a372a5da3217ecba24b5e
siddhiDhamnaskar <siddhidhamnaskar64@gmail.com>
2024-06-12 11:46:44 +0530

    time changed of testModes

--- src/helpers/socketServer.js
+++ src/helpers/socketServer.js
@@ -319,7 +319,7 @@ const server = net.createServer((socket) => {
             socket.write(`*TV?#`);
              setIntervalAndStore(() => {
               sendClear(socket,name);
-             },12000)
+             },10000)
           },2000)
        
         
@@ -341,7 +341,7 @@ const server = net.createServer((socket) => {
             socket.write(`*TV?#`);
              setIntervalAndStore(() => {
               sendVend(socket,TID++,name);
-            },5000)
+            },8000)
           },2000)
       
          
