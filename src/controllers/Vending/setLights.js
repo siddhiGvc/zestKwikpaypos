30fd57f1ba1788128d34793b981c7261e2d3a88b
siddhiGvc <siddhi@gvc.in>
2024-07-29 14:14:41 +0530

    minor change

--- src/controllers/Vending/setLights.js
+++ src/controllers/Vending/setLights.js
@@ -7,7 +7,7 @@ export const SetLights = async (req, res) => {
       const data=req.body;
       console.log(data);
       const juction=req.body.Junction;
-      var message="*R1:"+req.body.R1+","+"R2:"+req.body.R2+","+"R3:"+req.body.R3+"R4:"+req.body.R4+"#";
+      var message="*"+req.body.R1+","+req.body.R2+","+req.body.R3+","+req.body.R4+"#";
       mqttClient.sendMessage('GVC/TRA/' + juction,message);
       res.status(200).json("Okay");
     } catch (error) {
