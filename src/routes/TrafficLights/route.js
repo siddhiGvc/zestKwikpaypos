818ec62db4cdc8009b54bde625ed3cc1bd50f429
siddhiGvc <siddhi@gvc.in>
2024-08-02 12:46:55 +0530

    getlights api created

--- src/routes/TrafficLights/route.js
+++ src/routes/TrafficLights/route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { SetLights,SetDate,QueryPowreBackup } from '../../controllers/TrafficLights/setLights';
+import { SetLights,SetDate,QueryPowreBackup,getLights } from '../../controllers/TrafficLights/setLights';
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post('/setLights', SetLights);
 router.post('/setDate', SetDate);
 router.post('/queryPowerBackup', QueryPowreBackup);
+router.post('/getLights',getLights);
 
 
 module.exports = router;
