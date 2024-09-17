9ba5752164b915a0a01d3063e2f61b4fb4cb17c2
siddhi@gvc.in <siddhidhamnaskar6@gmail.com>
2024-09-17 16:30:28 +0530

    changes in routes

--- src/routes/TrafficLights/route.js
+++ src/routes/TrafficLights/route.js
@@ -1,28 +1,26 @@
 import express from 'express';
 // import { SetLights,SetDate,QueryPowreBackup,getLights,getQueryPowerBackup, GetAllJunctions, GetAllDevices, GetAllInverters} from '../../controllers/TrafficLights/setLights';
 
-import { sendMessage } from '../../controllers/Traffic/trafficMacAddress';
+import * as macAddress from '../../controllers/Traffic/trafficMacAddress';
 const router = express.Router();
 
 //= ===============================
 // Admin routes
 //= ===============================
-import * as macAddress from '../../controllers/Traffic/trafficMacAddress';
+
 
 
 router.get('/getMacAddress',macAddress.getAllMacAddress);
 router.get('/getData',macAddress.getData);
-router.post('/saveINHoutput',macAddress.saveINHoutput);
 router.post('/sendFota',macAddress.sendFota);
 router.post('/reset',macAddress.sendReset);
-router.post('/sendV',macAddress.sendV);
-router.post('/sendTC',macAddress.sendTC);
+
 router.post('/sendFW',macAddress.sendFW);
-router.post('/sendTV',macAddress.sendTV);
+
 router.post('/sendFotaUrl',macAddress.sendFotaUrl);
 router.post('/askUrl',macAddress.askUrl);
-router.post('/sendCC',macAddress.sendCC);
-router.post('/sendLight',macAddress.sendLight);
+
+
 router.post('/sendHBT',macAddress.sendHBT);
 router.post('/sendSIP',macAddress.sendSIP);
 router.post('/askSIP',macAddress.askSIP);
@@ -31,8 +29,7 @@ router.post('/askSSID',macAddress.askSSID);
 router.post('/sendPWD',macAddress.sendPWD);
 router.post('/sendSSID1',macAddress.sendSSID1);
 router.post('/sendPWD1',macAddress.sendPWD1);
-router.post('/sendCA',macAddress.sendCA);
-router.post('/askCA',macAddress.askCA);
+
 router.post('/modeTest1',macAddress.modeTest1);
 router.post('/modeTest2',macAddress.modeTest2);
 router.post('/modeTest3',macAddress.modeTest3);
@@ -43,12 +40,9 @@ router.post('/setSN',macAddress.setSN);
 router.post('/checkSN',macAddress.checkSN);
 router.post('/setErase',macAddress.setErase);
 router.post('/checkErase',macAddress.checkErase);
-router.post('/setPair',macAddress.setPair);
-router.post('/checkPair',macAddress.checkPair);
-router.post('/setL',macAddress.setL);
+
 router.get('/getSerialPorts',macAddress.getSerialPorts);
-router.post('/sendPassThru',macAddress.sendPassThru);
-router.post('/checkPassThru',macAddress.checkPassThru);
-router.post('/sendMessage', sendMessage);
+
+router.post('/sendMessage', macAddress.sendMessage);
 
 module.exports = router;
