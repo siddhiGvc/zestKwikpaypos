facd22757da3665125412a86ea41e8759c44a479
siddhiGvc <siddhi@gvc.in>
2024-10-10 11:44:38 +0530

    created new database of UnilineMacMapping

---
+++ src/migrations/20241010-0000-create-UnilineMacMapping.js
@@ -0,0 +1,83 @@
+
+
+module.exports = {
+    up: (queryInterface, Sequelize) => queryInterface.createTable('UnilineMacMapping', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      UID: {
+        
+        type: Sequelize.STRING,
+      },
+
+      MacID: {
+        
+        type: Sequelize.STRING,
+      },
+      Location: {
+        
+        type: Sequelize.STRING,
+      },
+      City: {
+        
+        type: Sequelize.STRING,
+      },
+      SocketNumber: {
+        
+        type: Sequelize.STRING,
+      },
+    
+      G1: {
+        type: Sequelize.STRING,
+      },
+      G2: {
+        type: Sequelize.STRING,
+      },
+      G3: {
+        type: Sequelize.STRING,
+      },
+      I: {
+        type: Sequelize.STRING,
+      },
+      GF: {
+        type: Sequelize.STRING,
+      },
+      Q: {
+        type: Sequelize.STRING,
+      },
+      Q1: {
+        type: Sequelize.STRING,
+      },
+      T: {
+        type: Sequelize.STRING,
+      },
+      TL: {
+        type: Sequelize.STRING,
+      },
+      S: {
+        type: Sequelize.STRING,
+      },
+      ST: {
+        type: Sequelize.STRING,
+      },
+      F: {
+        type: Sequelize.STRING,
+      },
+      SNoutput: {
+        type: Sequelize.STRING,
+      },
+   
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    }),
+    down: (queryInterface, Sequelize) => queryInterface.dropTable('MacMapping'),
+  };
