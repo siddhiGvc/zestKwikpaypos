f30d4151ea006b33025928b15c4b0f0194469dd3
siddhiGvc <siddhi@gvc.in>
2024-10-27 12:40:31 +0530

    ap and functons added for UnilineHourlyReport

--- daily_schedule.js
+++ daily_schedule.js
@@ -15,20 +15,20 @@ const setup = async function () {
 
 
 
-    const job = schedule.scheduleJob('00 19 * * *', function () {
-        console.log('Running Daily Schedule!');
-        dataController.CalcDailySummary().then(list => {
-            fs.appendFile(logPath, `[${moment().format()}]\nRunning Now\n${JSON.stringify(list)}\n\n`, err => {
-                //console.log(err)
-            });
-            console.log('Daily Schedule completed');
-            console.log('Archiving records');
-            dataController.ArchiveTransactions();
-            console.log('Archiving records completed');
-        });
-    });
+    // const job = schedule.scheduleJob('00 19 * * *', function () {
+    //     console.log('Running Daily Schedule!');
+    //     dataController.CalcDailySummary().then(list => {
+    //         fs.appendFile(logPath, `[${moment().format()}]\nRunning Now\n${JSON.stringify(list)}\n\n`, err => {
+    //             //console.log(err)
+    //         });
+    //         console.log('Daily Schedule completed');
+    //         console.log('Archiving records');
+    //         dataController.ArchiveTransactions();
+    //         console.log('Archiving records completed');
+    //     });
+    // });
     console.log('Daily Schedule initialized');
-    return job;
+    return jobHourlyReport;
 }
 
 module.exports = setup;
