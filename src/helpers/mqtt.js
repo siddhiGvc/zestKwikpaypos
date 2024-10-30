7d63d1a6323a7c1ddb094682cac9d18e1f63b2c0
siddhiGvc <siddhi@gvc.in>
2024-10-30 14:22:28 +0530

    changes in mqtt

--- src/helpers/mqtt.js
+++ src/helpers/mqtt.js
@@ -94,7 +94,7 @@ const parseInternal = async(payload, mqttClient,topic) => {
                           
                         }
 
-
+                        console.log(G1.sn, G2.sn, G3.sn,I.sn,GF.sn);
                         if(G1.sn==G2.sn && G2.sn==G3.sn &&G3.sn==I.sn && I.sn==GF.sn)
                         {
                            await UnilineTransactions.create({
