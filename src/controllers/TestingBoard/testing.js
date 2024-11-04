e2dbe75f91f22dadf1f880e3c353ba7866f7ec5a
siddhiGvc <siddhi@gvc.in>
2024-11-04 10:26:40 +0530

    changes in resport api

--- src/controllers/TestingBoard/testing.js
+++ src/controllers/TestingBoard/testing.js
@@ -1,6 +1,6 @@
 import { Op } from 'sequelize';
 import moment from 'moment';
-const {MacMapping,TestMode,SerialPort,UnilineMacMapping,UnilineTransactions} =require("../../models")
+const {MacMapping,TestMode,SerialPort,UnilineMacMapping,UnilineTransactions,sequelize} =require("../../models")
 import { successResponse, errorResponse, uniqueId } from '../../helpers';
 var events = require('../../helpers/events');
 const mqttHandler=require('../../../mqtt');
